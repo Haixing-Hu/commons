bf318773daef42fa71ad06501c6095d6140e204d
Haixing Hu <starfish.hu@gmail.com>
2014-09-10 22:49:54 +0800

    add some JAXB util classes

---
+++ src/main/java/com/github/haixing_hu/util/jaxb/DateXmlAdaptor.java
@@ -0,0 +1,62 @@
+/******************************************************************************
+ *
+ * Copyright (c) 2014  Haixing Hu
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ ******************************************************************************/
+
+package com.github.haixing_hu.util.jaxb;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.TimeZone;
+
+import javax.xml.bind.DatatypeConverter;
+import javax.xml.bind.annotation.adapters.XmlAdapter;
+
+/**
+ * The customized JAXB data type adaptor for the {@link Date} type.
+ * <p>
+ * This adaptor will print the {@link Date} type in the xsd:dateTime format
+ * using the UTC time zone, i.e., the epoch (<code>new Date(0)</code>) will be
+ * printed in the form of "1970-01-01T00:00:00Z".
+ *
+ * @author Haixing Hu
+ */
+public final class DateXmlAdaptor extends XmlAdapter<String, Date> {
+
+  private final TimeZone utc = TimeZone.getTimeZone("UTC");
+
+  @Override
+  public Date unmarshal(String v) throws Exception {
+    if (v == null) {
+      return null;
+    } else {
+      final Calendar cal = DatatypeConverter.parseDateTime(v);
+      return cal.getTime();
+    }
+  }
+
+  @Override
+  public String marshal(Date v) throws Exception {
+    if (v == null) {
+      return null;
+    } else {
+      final Calendar cal = new GregorianCalendar(utc);
+      cal.setTime(v);
+      return DatatypeConverter.printDateTime(cal);
+    }
+  }
+}
---
+++ src/main/java/com/github/haixing_hu/util/jaxb/EnumXmlAdapter.java
@@ -0,0 +1,105 @@
+/******************************************************************************
+ *
+ * Copyright (c) 2014  Haixing Hu
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ ******************************************************************************/
+
+package com.github.haixing_hu.util.jaxb;
+
+import javax.xml.bind.annotation.adapters.XmlAdapter;
+
+/**
+ * The customized JAXB data type adapter for the enumeration types.
+ * <p>
+ * This adapter will automatically change the style of the name of an
+ * enumerator, in the following way:
+ * <ul>
+ * <li>Lower case the name of the enumerator.</li>
+ * <li>Replace the '_' with '-'.</li>
+ * </ul>
+ * For example, suppose we have an enumeration class as follows:
+ * <code>
+ * <pre>
+ * package com.github.haixinghu.csl;
+ * public enum MyEnum {
+ *  FIRST,
+ *  SECOND_ITEM,
+ *  THE_THIRD_ITEM,
+ * }
+ * </pre>
+ * </code>
+ * We should create an adapter class extending the {@link EnumXmlAdapter} as
+ * follows:
+ * <code>
+ * <pre>
+ *  package com.github.haixinghu.csl;
+ *  public final class MyEnumXmlAdapter extends EnumXmlAdapter<MyEnum> {
+ *    public MyEnumXmlAdapter() {
+ *      super(MyEnum.class);
+ *    }
+ *  }
+ * </pre>
+ * </code>
+ * Then we should use a
+ * <a href="http://www.onjava.com/pub/a/onjava/2004/04/21/declarative.html?page=3">
+ * package level annotation</a> in the <code>package-info.java</code> as follows:
+ * <code>
+ * <pre>
+ *  &#64;XmlJavaTypeAdapters({
+ *    &#64;XmlJavaTypeAdapter(type=MyEnum.class, value=MyEnumXmlAdapter.class)
+ *  })
+ *  package com.github.haixinghu.csl;
+ *  import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
+ *  import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapters;
+ * </pre>
+ * </code>
+ * This will make the JAXB automatically converts the names of enumerators in
+ * the <code>MyEnum</code> enumeration class as follows:
+ * <code>
+ * <pre>
+ *  FIRST <==> "first"
+ *  SECOND_ITEM <==> "second-item"
+ *  THE_THIRD_ITEM <==> "third-item"
+ * </pre>
+ * </code>
+ *
+ * @author Haixing Hu
+ */
+public class EnumXmlAdapter<T extends Enum<T>> extends XmlAdapter<String, T> {
+
+  private final Class<T> cls;
+
+  protected EnumXmlAdapter(Class<T> cls) {
+    this.cls = cls;
+  }
+
+  @Override
+  public T unmarshal(String v) throws Exception {
+    if (v == null) {
+      return null;
+    } else {
+      return Enum.valueOf(cls, v.toUpperCase().replace('-', '_'));
+    }
+  }
+
+  @Override
+  public String marshal(T v) throws Exception {
+    if (v == null) {
+      return null;
+    } else {
+      return v.name().toLowerCase().replace('_', '-');
+    }
+  }
+}
