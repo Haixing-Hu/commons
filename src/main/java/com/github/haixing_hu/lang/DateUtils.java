656451559d087a949c90750755ae5e87b4fe2756
Haixing Hu <starfish.hu@gmail.com>
2016-01-16 15:19:27 +0800

    add some methods

--- src/main/java/com/github/haixing_hu/lang/DateUtils.java
+++ src/main/java/com/github/haixing_hu/lang/DateUtils.java
@@ -19,7 +19,11 @@ package com.github.haixing_hu.lang;
 import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.nio.ByteOrder;
+import java.sql.Time;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.TimeZone;
 
 import javax.annotation.Nullable;
 
@@ -45,6 +49,26 @@ public class DateUtils {
    * The default date format pattern.
    */
   public static final String DEFAULT_PATTERN = "yyyy-MM-dd HH:mm:ss";
+
+  /**
+   * The default date time format pattern.
+   */
+  public static final String DEFAULT_DATE_TIME_PATTERN = "yyyy-MM-dd HH:mm:ss z";
+
+  /**
+   * The default date format pattern.
+   */
+  public static final String DEFAULT_DATE_PATTERN = "yyyy-MM-dd";
+
+  /**
+   * The default time format pattern.
+   */
+  public static final String DEFAULT_TIME_PATTERN = "HH:mm:ss";
+
+  /**
+   * The UTC time zone.
+   */
+  public static final TimeZone UTC = TimeZone.getTimeZone("UTC");
 
   public static boolean toBoolean(@Nullable final Date value) {
     return (value == null ? BooleanUtils.DEFAULT : (value.getTime() != 0));
@@ -252,5 +276,74 @@ public class DateUtils {
   public static BigDecimal toBigDecimal(@Nullable final Date value,
       @Nullable final BigDecimal defaultValue) {
     return (value == null ? defaultValue : BigDecimal.valueOf(value.getTime()));
+  }
+
+  /**
+   * Creates a {@link java.sql.Date} for a specified date in the UTC time zone.
+   * <p>
+   * <b>NOTE:</b> the month is starting from 1 instead of 0.
+   *
+   * @param year
+   *          the year.
+   * @param month
+   *          the month, starting from 1.
+   * @param day
+   *          the day in the month, starting from 1.
+   * @return the {@link java.sql.Date} object corresponds to the specified date.
+   */
+  public static java.sql.Date getDate(final int year, final int month,
+                                      final int day) {
+    final Calendar cal = new GregorianCalendar(UTC);
+    cal.clear();
+    cal.set(year, month - 1, day);
+    return new java.sql.Date(cal.getTimeInMillis());
+  }
+
+  /**
+   * Creates a {@link java.util.Date} for a specified date time in the UTC time
+   * zone.
+   * <p>
+   * <b>NOTE:</b> the month is starting from 1 instead of 0.
+   *
+   * @param year
+   *          the year.
+   * @param month
+   *          the month, starting from 1.
+   * @param day
+   *          the day in the month, starting from 1.
+   * @param hour
+   *          the hour in the day, starting from 0.
+   * @param minute
+   *          the minute in the hour, starting from 0.
+   * @param second
+   *          the second in the minute, starting from 0.
+   * @return the {@link java.util.Date} object corresponds to the specified
+   *         date.
+   */
+  public static Date getDateTime(final int year, final int month,
+                                 final int day, final int hour, final int minute, final int second) {
+    final Calendar cal = new GregorianCalendar(UTC);
+    cal.clear();
+    cal.set(year, month - 1, day, hour, minute, second);
+    return cal.getTime();
+  }
+
+  /**
+   * Creates a {@link java.sql.Time} for a specified time in the UTC time zone.
+   *
+   * @param hour
+   *          the hour in the day, starting from 0.
+   * @param minute
+   *          the minute in the hour, starting from 0.
+   * @param second
+   *          the second in the minute, starting from 0.
+   * @return the {@link java.util.Date} object corresponds to the specified
+   *         date.
+   */
+  public static Time getTime(final int hour, final int minute, final int second) {
+    final Calendar cal = new GregorianCalendar(UTC);
+    cal.clear();
+    cal.set(1979, 0, 1, hour, minute, second);
+    return new Time(cal.getTimeInMillis());
   }
 }
